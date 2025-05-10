e1c4cff6cb23728f46eaea4eb50d0e2d7b0337ee
gongPyeon <pyeonkang@naver.com>
2025-05-10 21:11:29 +0900

    chore: DDD패턴에 기반한 패키지 구조 변경 및 build.gradle 주석 추가

---
+++ src/test/java/timetogeter/context/TimeTogetherApplicationTest.java
@@ -0,0 +1,11 @@
+package timetogeter.context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTogetherApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
