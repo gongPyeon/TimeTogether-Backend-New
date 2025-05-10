11b8bb38a51daf18443afe897a65aadd15be3760
gongPyeon <pyeonkang@naver.com>
2025-05-10 21:41:20 +0900

    chore(TimeTogetherApplicationTest): 위치 변경

--- src/test/java/timetogeter/TimeTogetherApplicationTest.java
+++ src/test/java/timetogeter/TimeTogetherApplicationTest.java
@@ -1,7 +1,9 @@
-package timetogeter.context;
+package timetogeter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TimeTogetherApplicationTest {
