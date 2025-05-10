672b67afd94f02aacfab54581acf41cc6e69aee9
gongPyeon <pyeonkang@naver.com>
2025-05-10 20:54:27 +0900

    chore: 기존 패키지 구조 삭제

--- src/main/java/com/pro/oauth2/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pro.oauth2.repository;
-
-import com.pro.oauth2.entity.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}
