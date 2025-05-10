6500dfa1c149a05bd71a956e36a8f7ba93c77249
gongPyeon <pyeonkang@naver.com>
2025-05-10 22:50:59 +0900

    chore(time): time 패키지 엔티티 구성

---
+++ src/main/java/timetogeter/context/time/domain/entity/Time.java
@@ -0,0 +1,27 @@
+package timetogeter.context.time.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Time {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int timeId;
+
+    private int dateId;
+
+    private LocalTime time;
+}
