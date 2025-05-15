baeb53978bbe310475875029d85f5b14f54195c6
gongPyeon <pyeonkang@naver.com>
2025-05-15 10:54:30 +0900

    feat(SpringSecurity): 기본 시큐리티 설정

--- src/main/java/timetogeter/global/security/config/SecurityConfig.java
+++ src/main/java/timetogeter/global/security/config/SecurityConfig.java
@@ -5,21 +5,55 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, CorsConfig corsConfig) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/docs/**", "/static/docs/**").permitAll()
-                        .anyRequest().permitAll()
-                )
-                .csrf(csrf -> csrf.disable());
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .logout(logout -> logout.disable())
+                .rememberMe(rememberMe -> rememberMe.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(requestPermitAll()).permitAll()
+                        .anyRequest().authenticated());
+
+        // TODO: filter 순서 설정
 
         return http.build();
+    }
+
+    private RequestMatcher[] requestPermitAll() {
+        List<RequestMatcher> requestMatchers = List.of(
+                antMatcher(GET, "/docs/**"),
+                antMatcher(POST, "/static/docs/**"), // API 문서
+                antMatcher(POST, "/auth/**"), // 회원가입 & 로그인
+                antMatcher(GET, "/test/**")); // 테스트 시 (선택)
+        return requestMatchers.toArray(RequestMatcher[]::new);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
