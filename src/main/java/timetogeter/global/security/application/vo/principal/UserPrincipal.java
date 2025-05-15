a7febe195850610167df6d934457d5329f17dd54
gongPyeon <pyeonkang@naver.com>
2025-05-15 13:23:37 +0900

    feat(UserPrincipal): 로그인된 사용자의 정보를 담는 객체 생성

--- src/main/java/timetogeter/global/security/application/vo/principal/UserPrincipal.java
+++ src/main/java/timetogeter/global/security/application/vo/principal/UserPrincipal.java
@@ -5,6 +5,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import timetogeter.context.auth.domain.vo.Provider;
+import timetogeter.context.auth.domain.vo.Role;
 import timetogeter.global.security.application.dto.RegisterResponse;
 
 import java.util.Collection;
@@ -13,5 +15,62 @@ import java.util.Map;
 
 @Getter
 public class UserPrincipal implements OAuth2User, UserDetails {
+
+    private final RegisterResponse registerResponse;
+    private final Collection<? extends GrantedAuthority> authorities;
+    private final Map<String, Object> attributes;
+
+    // 소셜 로그인
+    public UserPrincipal(RegisterResponse registerResponse, Map<String, Object> attributes) {
+        this.registerResponse = registerResponse;
+        this.attributes = attributes;
+        this.authorities = Collections.singletonList(
+                new SimpleGrantedAuthority(registerResponse.role().name())
+        );
+    }
+
+    // 일반 로그인
+    public UserPrincipal(RegisterResponse registerResponse) {
+        this.registerResponse = registerResponse;
+        this.attributes = null;
+        this.authorities = Collections.singletonList(
+                new SimpleGrantedAuthority(registerResponse.role().name())
+        );
+    }
+
+    // TODO: 각각 null 일 경우 예외처리를 해놓을지 고민
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public String getId(){
+        return registerResponse.userId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return registerResponse.password();
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return registerResponse.nickname();
+    }
+
+    public Provider getProvider() { return registerResponse.provider(); }
+    public Role getRole() {
+        return registerResponse.role();
+    }
 
 }
