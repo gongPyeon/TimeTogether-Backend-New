cf9cd3609ae69122d2c559c355b502094f87a14b
gongPyeon <pyeonkang@naver.com>
2025-06-08 03:35:07 +0900

    fix(GroupInfoService): groupName을 조회하는 함수 제거

--- src/main/java/timetogeter/context/schedule/application/service/ConfirmedScheduleService.java
+++ src/main/java/timetogeter/context/schedule/application/service/ConfirmedScheduleService.java
@@ -2,7 +2,6 @@ package timetogeter.context.schedule.application.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import timetogeter.context.group.application.service.GroupInfoService;
 import timetogeter.context.schedule.application.dto.PromiseResDTO;
 import timetogeter.context.schedule.application.dto.request.GetPromiseBatchReqDTO;
 import timetogeter.context.schedule.application.dto.response.PromiseDetailResDTO;
@@ -21,7 +20,6 @@ import java.util.stream.Collectors;
 public class ConfirmedScheduleService {
 
     private final ScheduleRepository scheduleRepository;
-    private final GroupInfoService groupInfoService;
     private final PromiseShareKeyRepository promiseShareKeyRepository;
 
 
@@ -46,13 +44,12 @@ public class ConfirmedScheduleService {
         Schedule schedule = scheduleRepository.findById(scheduleId)
                 .orElseThrow(() -> new ScheduleNotFoundException(BaseErrorCode.SCHEDULE_NOT_FOUND, "[ERROR} " + scheduleId + "에 해당하는 일정이 존재하지 않습니다."));
 
-        String groupName = groupInfoService.getGroupName(schedule.getGroupId());
         // TODO: PromiseId는 별도의 테이블에서 관리해야함 -> Schedule에서 Promise에 접근할 수 있도록
         List<String> names = promiseShareKeyRepository.findByPromiseId(schedule.getPromiseId()).stream()
                 .map(s -> s.getEncUserId())
                 .collect(Collectors.toList());
 
-        return new PromiseDetailResDTO(scheduleId, schedule.getTitle(), schedule.getType(), schedule.getPlace(), groupName, names);
+        return new PromiseDetailResDTO(scheduleId, schedule.getTitle(), schedule.getType(), schedule.getPlace(), schedule.getGroupName(), names);
     }
 
     public PromiseListResDTO searchPromiseView(String query, List<String> filter) {
