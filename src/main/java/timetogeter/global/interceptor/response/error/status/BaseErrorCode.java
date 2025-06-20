d1ad38a63567e6b14e5ad644be83169a5cdb4ae4
gongPyeon <pyeonkang@naver.com>
2025-06-21 07:19:20 +0900

    refactor: BaseErrorCode, BaseCode 메세지 형식 개선

--- src/main/java/timetogeter/global/interceptor/response/error/status/BaseErrorCode.java
+++ src/main/java/timetogeter/global/interceptor/response/error/status/BaseErrorCode.java
@@ -25,15 +25,16 @@ public enum BaseErrorCode implements StatusCode {
     INVALID_ID_LENGTH(400, HttpStatus.BAD_REQUEST, "아이디는 1자 이상 20자 이하로 입력해주세요"),
     INVALID_ID_FORMAT(400, HttpStatus.BAD_REQUEST, "아이디 형식을 다시 확인해주세요"),
     INVALID_ID_DUP(400, HttpStatus.BAD_REQUEST, "다른 아이디를 사용해주세요"),
-    INVALID_NICKNAME_LENGTH(400, HttpStatus.BAD_REQUEST, "아이디는 1자 이상 20자 이하로 입력해주세요"),
+    INVALID_NICKNAME_LENGTH(400, HttpStatus.BAD_REQUEST, "닉네임은 1자 이상 20자 이하로 입력해주세요"),
     INVALID_NICKNAME_FORMAT(400, HttpStatus.BAD_REQUEST, "닉네임 형식을 다시 확인해주세요"),
     INVALID_EMAIL_FORMAT(400, HttpStatus.BAD_REQUEST, "이메일 형식을 다시 확인해주세요"),
     INVALID_PHONE_FORMAT(400, HttpStatus.BAD_REQUEST, "전화번호 형식을 다시 확인해주세요"),
+
     INVALID_TOKEN_REDIS(400, HttpStatus.BAD_REQUEST, "아이디에 해당하는 리프레시 토큰이 존재하지 않아요"),
     MISMATCH_TOKEN_REDIS(400, HttpStatus.BAD_REQUEST, "아이디에 해당하는 리프레시 토큰과 일치하지 않아요"),
     FAIL_LOGIN(401, HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호를 확인해주세요"),
     ACCOUNT_LOCKED(401, HttpStatus.UNAUTHORIZED, "계정이 잠겨 있습니다. 나중에 다시 시도하세요."),
-    REDIS_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "세션 저장에 실패했어요"),
+    REDIS_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "세션 처리에 실패했어요"),
 
     SCHEDULE_NOT_FOUND(404, HttpStatus.NOT_FOUND, "일정이 존재하지 않아요"),
     GROUP_NOT_FOUND(404, HttpStatus.NOT_FOUND, "그룹이 존재하지 않아요"),
