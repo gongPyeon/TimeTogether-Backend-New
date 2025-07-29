8b77c20fb08139fb74541b736afd2794fecaf75a
Hyeri1ee <haerizian10@gmail.com>
2025-07-29 16:32:10 +0900

    chore: 배포 테스트 6-5

--- src/main/java/timetogeter/global/config/RedisConfig.java
+++ src/main/java/timetogeter/global/config/RedisConfig.java
@@ -13,9 +13,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableCaching
 public class RedisConfig {
-    @Value("${spring.redis.connection.host}")
+    @Value("${spring.redis.connection.host=meetnow-redis}")
     private String host;
-    @Value("${spring.redis.connection.port}")
+    @Value("${spring.redis.connection.port=6379}")
     private int port;
 
     @Bean
