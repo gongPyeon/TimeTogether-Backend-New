4fd65e46fcc1cd590ad28f19c38d4eccaeed6699
Hyeri1ee <haerizian10@gmail.com>
2025-08-12 23:29:46 +0900

    chore: 오토컨피그 제거

--- src/main/java/timetogeter/TimeTogetherApplication.java
+++ src/main/java/timetogeter/TimeTogetherApplication.java
@@ -2,7 +2,9 @@ package timetogeter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration.class
+})
 public class TimeTogetherApplication {
     public static void main(String[] args) {
         SpringApplication.run(TimeTogetherApplication.class, args);
