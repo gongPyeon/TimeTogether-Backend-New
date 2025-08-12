69d0510cee4382b2fb0e78b83882106ff310d0f2
Hyeri1ee <haerizian10@gmail.com>
2025-08-12 23:03:22 +0900

    chore: FileDescriptorMetrics 호환성 문제

--- src/main/java/timetogeter/global/config/MetricsConfig.java
+++ src/main/java/timetogeter/global/config/MetricsConfig.java
@@ -1,15 +1,18 @@
 package timetogeter.global.config;
 
+import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class MetricsConfig {
 
     @Bean
-    public ProcessorMetrics processorMetrics() {
-        // 빈 등록하지 않도록 막음
-        return null;
+    @ConditionalOnProperty(name = "management.metrics.enable.jvm.file.descriptor", havingValue = "true", matchIfMissing = true)
+    public FileDescriptorMetrics fileDescriptorMetrics() {
+        return new FileDescriptorMetrics();
     }
 }
