29d1a46b7ee5ceffabb36ef3e4ab14383800f0ee
gongPyeon <pyeonkang@naver.com>
2025-12-18 21:25:55 +0900

    feat: 약속 조회 시 별점 등록 여부 반환 추가

--- src/main/java/timetogeter/context/place/domain/repository/UserBoardRepository.java
+++ src/main/java/timetogeter/context/place/domain/repository/UserBoardRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.repository.query.Param;
 import timetogeter.context.place.domain.entity.PromisePlace;
 import timetogeter.context.place.domain.entity.UserBoard;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserBoardRepository extends JpaRepository<UserBoard, String> {
 
     @Query("SELECT u FROM UserBoard u WHERE u.placeBoardId = :placeBoardId AND u.userId = :userId")
     Optional<UserBoard> findByPlaceBoardIdAndUserId(@Param("placeBoardId") int placeBoardId, @Param("userId")String userId);
+
+    @Query("SELECT ub.placeBoardId FROM UserBoard ub WHERE ub.userId = :userId AND ub.placeBoardId IN :placeBoardIds")
+    Set<Integer> findRatedBoardIds(@Param("userId") String userId, @Param("placeBoardIds") List<Integer> placeBoardIds);
 }
---
+++ src/main/java/timetogeter/context/schedule/application/dto/request/PromiseSearchReqDTO.java
@@ -0,0 +1,4 @@
+package timetogeter.context.schedule.application.dto.request;
+
+public record PromiseSearchReqDTO(String pseudoId) {
+}
